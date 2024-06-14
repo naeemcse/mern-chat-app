80101689afa6e65dd771f4768e9c7ba69480428f
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2024-06-14 23:49:48 +0600

    dp.js is edited for uri being as string

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 var colors = require('colors');
 const connectDB = async ()=>{
+    const uri = process.env.MONGODB_URI || "mongodb+srv://csenajmulislamnaeem:csenaeem019@cluster0.aqqhics.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     try{
-        const conn = await  mongoose.connect(process.env.MONGODB_URI,{
+        const conn = await  mongoose.connect(uri,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
